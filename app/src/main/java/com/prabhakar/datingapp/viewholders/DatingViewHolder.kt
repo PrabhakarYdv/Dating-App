777ad81bf5946bc86ef3c2743d51f7bc36feaef8
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-09-01 19:36:10 +0530

    Card Stack View Done

--- app/src/main/java/com/prabhakar/datingapp/viewholders/DatingViewHolder.kt
+++ app/src/main/java/com/prabhakar/datingapp/viewholders/DatingViewHolder.kt
@@ -5,7 +5,7 @@ import com.bumptech.glide.Glide
 import com.prabhakar.datingapp.databinding.ItemUserLayoutBinding
 import com.prabhakar.datingapp.model.UserModel
 
-class DatingViewHolder(val binding: ItemUserLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+class DatingViewHolder(private val binding: ItemUserLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
     fun setData(model: UserModel) {
         binding.apply {
             name.text = model.userName
