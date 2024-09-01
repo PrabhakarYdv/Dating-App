4bc37f05d3a3a9c70f05932b008ef79ff47bec81
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-09-01 07:57:57 +0530

    Bottom Navigation

--- app/src/main/java/com/prabhakar/datingapp/view/fragment/MessageFragment.kt
+++ app/src/main/java/com/prabhakar/datingapp/view/fragment/MessageFragment.kt
@@ -7,23 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.prabhakar.datingapp.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MessageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MessageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(com.prabhakar.datingapp.ARG_PARAM1)
-            param2 = it.getString(com.prabhakar.datingapp.ARG_PARAM2)
-        }
-    }
+class MessageFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,23 +19,4 @@ class MessageFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_message, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MessageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MessageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(com.prabhakar.datingapp.ARG_PARAM1, param1)
-                    putString(com.prabhakar.datingapp.ARG_PARAM2, param2)
-                }
-            }
-    }
 }
