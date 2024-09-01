eaf5ca1e3f1490eeba4e14decfabe18ca88a5b6c
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-09-01 09:08:04 +0530

    Navigation Drawer

--- app/src/main/java/com/prabhakar/datingapp/view/activity/HomeActivity.kt
+++ app/src/main/java/com/prabhakar/datingapp/view/activity/HomeActivity.kt
@@ -1,17 +1,86 @@
 package com.prabhakar.datingapp.view.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.prabhakar.datingapp.R
 import com.prabhakar.datingapp.Utils
 import com.prabhakar.datingapp.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityHomeBinding
+    private var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         Utils.setStatusBarColor(this, R.color.red)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle!!)
+        actionBarDrawerToggle!!.syncState()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favourite -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+
+            R.id.rate -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+
+            R.id.share -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+
+            R.id.termsAndCondition -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+
+            R.id.privacy -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+
+            R.id.about -> {
+                Utils.showToast(this, "Coming Soon")
+            }
+        }
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle!!.onOptionsItemSelected(item)) {
+            true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.close()
+        } else {
+            super.onBackPressed()
+
+        }
     }
 }
