be23fde79b2aa39397bfc420415a10a9c3cb3064
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-09-05 14:44:18 +0530

    Profile UI

--- app/src/main/java/com/prabhakar/datingapp/view/fragment/ProfileFragment.kt
+++ app/src/main/java/com/prabhakar/datingapp/view/fragment/ProfileFragment.kt
@@ -6,16 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.prabhakar.datingapp.R
+import com.prabhakar.datingapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
+private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+       binding =FragmentProfileBinding.inflate(layoutInflater)
+
+
+        return binding.root
     }
 
 
